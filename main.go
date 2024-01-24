bbaec6d66e4bf4d54d34b06abf1da12d974eedb3
midoon <harunkoding@gmail.com>
2024-01-24 10:58:18 +0700

    day-2

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/midoon/codewars-wpu-challenge/kata"
+)
+
+func main() {
+	// fmt.Println(kata.Multiple3And5(10))
+	fmt.Println(kata.PrinterError("aaammm"))
+}
